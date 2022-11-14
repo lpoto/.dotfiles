45a082685674bf428d163c3a0e020d437e1a3c9c
lpoto <luka.potochnik@gmail.com>
2022-11-14 15:51:22 +0100

    Remove all netrw plugins

--- init.lua
+++ init.lua
@@ -14,8 +14,6 @@ vim.g["mapleader"] = " " -- map <leader> to space
 
 ---Set the default global options for the editor
 ---(save/undo, indenting, autocomplete, searching, ui)
-vim.opt.filetype = "on" -- enable filetype detection
-vim.opt.syntax = "on" -- enable syntax highlighting
 vim.opt.errorbells = false -- disable error sounds
 vim.opt.updatetime = 50 -- shorten updatetime from 4s to 50ms
 vim.opt.timeoutlen = 500 -- shorten timeout for key combinations
@@ -84,9 +82,10 @@ vim.opt.statusline = ""
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
 
 ----------------------------------------------------------------------- PLUGINS
 -- NOTE: plugins are defined in lua/plugins/, call the setup
 -- function that configures them.
-
 require("plugins").setup()
