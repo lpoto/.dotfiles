4c41e38c1fc3ae98b7d9849f410703b99399c25d
lpoto <luka.potochnik@gmail.com>
2022-11-14 04:25:52 +0100

    Add descriptive comments for local config

--- after/plugin/add_local_config.lua
+++ after/plugin/add_local_config.lua
@@ -1,45 +1,50 @@
 --=============================================================================
 -------------------------------------------------------------------------------
---                                                             ADD LOCAL CONFIG
+--                                                                 LOCAL CONFIG
 --=============================================================================
--- Search if there is an existing local .nvim/ directory relative to the
--- current project, if there is, source it.
+-- Store local project configs in .local/ directory.
+-- Configs are saved based on the root of the currently oppened project and
+-- the current machine's hostname. Local configs specific to filetypes
+-- may also be added.
+-- Open/ add local configs with :LocalConfig <filetype>  (filetype is optional)
+-- Remove them with :RemoveLocalConfig <filetype>  (filetype is optional)
 --_____________________________________________________________________________
 
----Gets the base of the path for the local config based on the root
----of the currently oppened project.
----The returned path is missing .<filetype>.lua or just .lua at the end.
----
----@return string: base of the path
----@return string: escaped base of the path
-local function get_local_config_path_base()
-  local local_configs_path = vim.fn.stdpath "config" .. "/.local"
-  local root = require "util.root"()
-  local base = local_configs_path .. "/" .. vim.fn.hostname() .. "/"
-  local path_escaped = base
-    .. vim.fn.substitute(
-      vim.fn.substitute(root, "\\", "\\\\%", "g"),
-      "/",
-      "\\\\%",
-      "g"
-    )
-  local path = base
-    .. vim.fn.substitute(
-      vim.fn.substitute(root, "\\", "\\%", "g"),
-      "/",
-      "\\%",
-      "g"
-    )
-  return path, path_escaped
-end
+---The root of the currently oppened project
+---@type string
+local root = require "util.root"()
+---The defult path for the local config files
+---@type string
+local local_configs_path = vim.fn.stdpath "config" .. "/.local"
+---The base of the local config files for the current host
+---@type string
+local base = local_configs_path .. "/" .. vim.fn.hostname() .. "/"
+---The escaped base of the current local project file
+---@type string
+local path_escaped = base
+  .. vim.fn.substitute(
+    vim.fn.substitute(root, "\\", "\\\\%", "g"),
+    "/",
+    "\\\\%",
+    "g"
+  )
+---The uneescaped base of the current local project file
+---@type string
+local path = base
+  .. vim.fn.substitute(
+    vim.fn.substitute(root, "\\", "\\%", "g"),
+    "/",
+    "\\%",
+    "g"
+  )
 
 ---Opens local config for the currently oppened project.
 ---If it does not exist, it prompts the option to create it.
 ---If a filetype is provided, it creates a project local config for
 ---the filetype.
+---@param filetype string?
 local function open_local_config(filetype)
-  local root = require "util.root"()
-  local file, file_escaped = get_local_config_path_base()
+  local file, file_escaped = path, path_escaped
   if filetype ~= nil and string.len(filetype) > 0 then
     file = file .. "." .. filetype
     file_escaped = file_escaped .. "." .. filetype
@@ -76,13 +81,14 @@ local function open_local_config(filetype)
   end
 end
 
----Opens local config for the currently oppened project.
----If it does not exist, it prompts the option to create it.
----If a filetype is provided, it creates a project local config for
+---Removes local config for the currently oppened project.
+---If it does not exist, it sends a warning.
+---If a filetype is provided, it removes a project local config for
 ---the filetype.
+---This prompts for confirmation before removing the file.
+---@param filetype string?
 local function remove_local_config(filetype)
-  local root = require "util.root"()
-  local file, file_escaped = get_local_config_path_base()
+  local file, file_escaped = path, path_escaped
   if filetype ~= nil and string.len(filetype) > 0 then
     file = file .. "." .. filetype
     file_escaped = file_escaped .. "." .. filetype
@@ -123,8 +129,12 @@ end
 
 local sourced = {}
 
+---Check if there is a local config file matching the root
+---of the currently oppened project. Also source the file matching
+---both the root and the current filetype.
+---Do not source if the file has already been sourced.
 local function source_local_configs()
-  local file, file_escaped = get_local_config_path_base()
+  local file, file_escaped = path, path_escaped
   local file_ft = file .. "." .. vim.o.filetype .. ".lua"
   local file_escaped_ft = file_escaped .. "." .. vim.o.filetype .. ".lua"
   file = file .. ".lua"
@@ -151,6 +161,8 @@ local function source_local_configs()
   end
 end
 
+---NOTE: use :LocalConfig  <ft>  command to open or create new
+---project local config files, that will be saved in the .local directory.
 vim.api.nvim_create_user_command("LocalConfig", function(o)
   if o.fargs ~= nil and next(o.fargs) ~= nil then
     open_local_config(o.fargs[1])
@@ -160,6 +172,8 @@ vim.api.nvim_create_user_command("LocalConfig", function(o)
 end, {
   nargs = "*",
 })
+---NOTE: use :RemoveLocalConfig  <ft>  command to remove existing
+---project local config files, be saved in the .local directory.
 vim.api.nvim_create_user_command("RemoveLocalConfig", function(o)
   if o.fargs ~= nil and next(o.fargs) ~= nil then
     remove_local_config(o.fargs[1])
@@ -170,10 +184,16 @@ end, {
   nargs = "*",
 })
 
+--NOTE: when oppening a new filetype, try to source the
+--default local config file and the local config file for the oppened
+--filetype, if they have not yet been sourced.
 vim.api.nvim_create_augroup("LocalConfig", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   group = "LocalConfig",
   callback = source_local_configs,
 })
 
-source_local_configs()
+--NOTE: source the local configs when entering neovim
+vim.api.nvim_exec_autocmds("FileType", {
+  group = "LocalConfig",
+})
