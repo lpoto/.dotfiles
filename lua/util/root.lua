0988007c700656b1037cbd23600e3dea1c2b354c
lpoto <luka.potochnik@gmail.com>
2022-11-14 05:06:14 +0100

    Update root util

--- lua/util/root.lua
+++ lua/util/root.lua
@@ -5,23 +5,18 @@
 -- Config for finding a project's root directory
 --_____________________________________________________________________________
 
-local function get_root(root_dir_patterns)
-  if type(root_dir_patterns) ~= "table" then
-    if vim.fn.exists "g:root_dir_patterns" == 1 then
-      root_dir_patterns = vim.g["root_dir_patterns"]
-    else
-      root_dir_patterns = {
-        ".git",
-        ".nvim",
-        "package.json",
-        "mvnw",
-        ".opam",
-        "GemFile",
-        ".nvim.root",
-      }
-    end
+---Find the root directory of the current project.
+---Searches for the first directory in the path that contains one
+---provided patterns.
+---@param root_dir_patterns table?: (default: {'.git', '.nvim.root'})
+---@param max_depth number:  max depth to check for root (default: 10)
+local function get_root(root_dir_patterns, max_depth)
+  if type(root_dir_patterns) ~= "table" or next(root_dir_patterns) == nil then
+    root_dir_patterns = { ".git", ".nvim.root", ".nvim" }
   end
-  local max_depth = 30
+  if type(max_depth) ~= "number" then
+    max_depth = 20
+  end
   if vim.g["root_dir_max_depth"] then
     max_depth = vim.g["root_dir_max_depth"]
   end
@@ -29,6 +24,9 @@ local function get_root(root_dir_patterns)
   local path_maker = default_path_maker
   for _ = 1, max_depth, 1 do
     local path = vim.fn.expand(path_maker)
+    if string.len(path) == 1 or path .. "/" == vim.fn.expand "~/" then
+      break
+    end
     for _, pattern in ipairs(root_dir_patterns) do
       if
         vim.fn.filereadable(path .. "/" .. pattern) == 1
@@ -36,9 +34,6 @@ local function get_root(root_dir_patterns)
       then
         return path
       end
-    end
-    if string.len(path) == 1 then
-      break
     end
     path_maker = path_maker .. ":h"
   end
