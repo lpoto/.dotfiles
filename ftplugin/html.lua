31c463df75c6d9940e8a1e762ff7a2b4e52c409e
Luka Potočnik <67372390+lpoto@users.noreply.github.com>
2022-11-15 16:44:18 +0100

    Html xhtml (#15)
    * Add formatter for html
    
    * Add formatter for xhtml

---
+++ ftplugin/html.lua
@@ -0,0 +1,34 @@
+--=============================================================================
+-------------------------------------------------------------------------------
+--                                                                         HTML
+--=============================================================================
+-- Loaded when a html file is oppened.
+--_____________________________________________________________________________
+
+----------------------------------------------------------------------- OPTIONS
+-- NOTE: set default tab width for xhtml
+
+vim.opt.tabstop = 2 -- set the width of a tab to 2
+vim.opt.softtabstop = 2 -- set the number of spaces that a tab counts for
+vim.opt.shiftwidth = 2 -- number of spaces used for each step of indent
+
+--------------------------------------------------------------------- FORMATTER
+-- NOTE: set prettier as default html formatter
+
+require("plugins.formatter").distinct_setup("html", {
+  -- npm install -g prettier
+
+  filetype = {
+    html = {
+      function()
+        return {
+          exe = "prettier",
+          args = {
+            vim.fn.expand "%:p",
+          },
+          stdin = true,
+        }
+      end,
+    },
+  },
+})
