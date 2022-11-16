ea95c35bd78e689e622a10158ca46cbfb69e2f72
Luka Potočnik <luka.potochnik@gmail.com>
2022-11-16 05:20:54 +0100

    Allow icons for lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,9 +9,9 @@
 return require("lualine").setup {
   options = {
     theme = "gruvbox-material",
-    icons_enabled = false,
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
+    icons_enabled = true,
+    --component_separators = { left = "", right = "" },
+    --section_separators = { left = "", right = "" },
   },
   sections = {
     lualine_a = { "mode" },
