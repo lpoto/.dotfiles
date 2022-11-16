4487e823be6be4ad13f7ac8c027bc783e43c5c34
Luka Potočnik <luka.potochnik@gmail.com>
2022-11-16 05:23:37 +0100

    Disable lualine separators

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,8 +10,8 @@ return require("lualine").setup {
   options = {
     theme = "gruvbox-material",
     icons_enabled = true,
-    --component_separators = { left = "", right = "" },
-    --section_separators = { left = "", right = "" },
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
   },
   sections = {
     lualine_a = { "mode" },
