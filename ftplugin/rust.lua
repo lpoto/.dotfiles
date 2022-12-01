c5d5c32d6409684c2fadbece84d3e6484025cb3e
Luka Potočnik <luka.potochnik@gmail.com>
2022-12-01 11:20:49 +0100

    Add rust config

---
+++ ftplugin/rust.lua
@@ -0,0 +1,56 @@
+--=============================================================================
+-------------------------------------------------------------------------------
+--                                                                         RUST
+--=============================================================================
+-- Loaded when a rust file is oppened.
+--_____________________________________________________________________________
+
+--------------------------------------------------------------------- LSPCONFIG
+-- NOTE: set rust-analyzer the default lsp server for rust
+
+--[[ rustup update ]]
+--[[ rustup component add rust-analyzer ]]
+-- make sure to export path to rust-analyzer so command "rust-analyzer" exists.
+require("plugins.lspconfig").distinct_setup("rust", function()
+  require("lspconfig").rust_analyzer.setup {
+    capabilities = require("plugins.cmp").default_capabilities(),
+    root_dir = function()
+      return require("util").get_root { ".git", ".gitignore", "Cargo.toml" }
+    end,
+  }
+  -- NOTE: Start the lsp server
+  vim.fn.execute("LspStart", true)
+end)
+
+--------------------------------------------------------------------- FORMATTER
+-- NOTE: set rust as the default formatter for rust
+
+require("plugins.formatter").distinct_setup("rust", {
+  filetype = {
+    rust = {
+      function()
+        return {
+          exe = "rustfmt",
+          stdin = true,
+        }
+      end,
+    },
+  },
+})
+
+----------------------------------------------------------------------- ACTIONS
+-- NOTE: set default actions
+
+require("plugins.actions").distinct_setup("rust", {
+  actions = {
+    -- Run currently oppened rust file
+    ["Run current Rust file"] = function()
+      return {
+        filetypes = { "rust" },
+        steps = {
+          { "rustc", vim.fn.expand "%:p" },
+        },
+      }
+    end,
+  },
+})
