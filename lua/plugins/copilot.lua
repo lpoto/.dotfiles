8364e77232926b55495a9e597a1546d0e4c8892c
lpoto <luka.potochnik@gmail.com>
2022-12-10 21:48:54 +0100

    Add disable function

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -7,6 +7,9 @@
 
 local M = {}
 
+---@type table: A table of disabled filetypes
+M.disabled_filetypes = {}
+
 ---The init function for the github copilot.
 ---This disables the default tab mapping for the plugin.
 ---Copilot is disabled for all filetypes by default.
@@ -57,26 +60,70 @@ function M.remappings()
   )
 end
 
----Enable copilot for the current filetype.
+---Enable copilot for the provided filetype, or the
+---current filetype if none is provided.
 ---If the copilot is not already running, run it.
 ---NOTE: by default copilot is disabled for all filetypes
 ---so it will not work for filetype until this is called.
 ---NOTE: this will be a noop if the :Copilot command is not available.
 ---Meaning, disabling the plugin will also disable this function.
-function M.enable()
+---NOTE: This will not work if copilot was disabled for the filetype
+---with `require('plugins.copilot').disable()`.
+---@param filetype string?: the filetype to enable copilot for.
+---If this is not provided, it is enabled for current filetype.
+function M.enable(filetype)
   if vim.fn.exists ":Copilot" == 0 then
     return
   end
+  if filetype == nil then
+    filetype = vim.bo.filetype
+  end
+  -- NOTE: if the copilot is disabled for the current filetype,
+  -- return early.
+  if M.disabled_filetypes[filetype] == true then
+    return
+  end
+  -- NOTE: make sure the copilot is enabled for the current filetype.
   if vim.g.copilot_filetypes == nil then
-    vim.g.copilot_filetypes = { [vim.o.filetype] = true }
+    vim.g.copilot_filetypes = { [filetype] = true }
   else
     vim.g.copilot_filetypes =
       vim.tbl_extend("force", vim.g.copilot_filetypes, {
-        [vim.o.filetype] = true,
+        [filetype] = true,
       })
   end
   if vim.g.loaded_copilot ~= 1 then
     vim.cmd "Copilot enable"
+  end
+end
+
+---Disable copilot for the provided filetype, or the
+---current filetype if none is provided.
+---This will disable the function `require('plugins.copilot').enable()`
+---for the provided filetype.
+---NOTE: this will be a noop if the :Copilot command is not available.
+---@param filetype string?: the filetype to disable copilot for.
+function M.disable(filetype)
+  if vim.fn.exists ":Copilot" == 0 then
+    return
+  end
+  if filetype == nil then
+    filetype = vim.bo.filetype
+  end
+
+  --NOTE: add it to disabled filetypes so that
+  --it cannot be enabled with the enable function.
+  M.disabled_filetypes[filetype] = true
+
+  --NOTE: add it do the copilot filetypes so that
+  --the copilot will not be enabled for the filetype.
+  if vim.g.copilot_filetypes == nil then
+    vim.g.copilot_filetypes = { [filetype] = false }
+  else
+    vim.g.copilot_filetypes =
+      vim.tbl_extend("force", vim.g.copilot_filetypes, {
+        [filetype] = false,
+      })
   end
 end
 
