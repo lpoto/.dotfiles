abfa1b939e45dd8c46632cb9b60f4592b6d0b111
lpoto <luka.potochnik@gmail.com>
2022-12-14 23:48:27 +0100

    Update plugin config, allow disabling

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,3 +1,14 @@
+--=============================================================================
+-------------------------------------------------------------------------------
+--                                                                       PLUGIN
+--=============================================================================
+--  A plugin object that stores plugin configs that are parsed by Packer.nvim.
+--  This allows dynamic definition of plugins, adding configs and disabling
+--  the plugins.
+--  NOTE: Once the plugin is loaded, it may not be disabled. Configs and may
+--  actions may still be added though.
+--_____________________________________________________________________________
+
 local log = require "log"
 
 local plugins = {}
@@ -8,14 +19,22 @@ local plugins = {}
 local Plugin = {}
 Plugin.__index = Plugin
 
+---Run the packer.nvim's use function on all the
+---enabled plugins.
+---
 ---@param use function: Packer.nvim's use function
 function Plugin.use(use)
   for _, plugin in pairs(plugins) do
-    use(plugin.__packer)
+    if plugin.__meta.disabled ~= true then
+      use(plugin.__packer)
+    end
   end
 end
 
----@return Plugin
+---Create a new plugin with the packer.nvim's config.
+---Not the `as` field should always be set.
+---
+---@return table: Packer.nvim's config table.
 function Plugin.new(o)
   local plugin = {
     __packer = {},
@@ -75,6 +94,9 @@ function Plugin.new(o)
   return plugin
 end
 
+---Get the plugin identified by the provided name.
+---The name should match the `as` field in the config.
+---
 ---@param name string
 ---@return Plugin
 function Plugin.get(name)
@@ -89,8 +111,15 @@ end
 ---be called after the plugin is loaded, alongside
 ---the config function added when defining the plugin.
 ---
+---If the plugin is already loaded, the function will be called
+---immediately.
+---
 ---@param f function: Config function
 function Plugin:config(f)
+  if package.loaded[self.__packer.as] then
+    f()
+    return
+  end
   if self.__meta.configs == nil then
     self.__meta.configs = {}
   end
@@ -113,6 +142,12 @@ function Plugin:run(key, ...)
   if self.__meta.actions[key] then
     self.__meta.actions[key](...)
   end
+end
+
+---Disable the plugin.
+---The plugin then won't be passed to Packer.nvim.
+function Plugin:disable()
+  self.__meta.disabled = true
 end
 
 return Plugin
