5f4d3ef92c82698d605fef83380b04fd31eb790d
lpoto <luka.potochnik@gmail.com>
2023-01-17 23:17:53 +0100

    Update toggled terminal appearance

--- lua/util/toggle_terminal.lua
+++ lua/util/toggle_terminal.lua
@@ -3,47 +3,61 @@
 --                                                              TOGGLE TERMINAL
 --=============================================================================
 
-return function()
-  if
-    vim.g.term_toggle_buf and vim.api.nvim_buf_is_valid(vim.g.term_toggle_buf)
-  then
-    local winid = vim.fn.bufwinid(vim.g.term_toggle_buf)
-    if winid and winid ~= -1 then
-      vim.api.nvim_buf_call(vim.g.term_toggle_buf, function()
-        local ok, e = pcall(vim.api.nvim_exec, "hide", false)
-        if ok == false then
-          vim.notify(e, vim.log.levels.ERROR)
-        end
-      end)
-      return
-    end
+local M = {}
 
-    local ok, e = pcall(vim.api.nvim_exec, "keepjumps vertical new", false)
-    if ok == false then
-      vim.notify(e, vim.log.levels.ERROR)
-      return
-    end
-    ok, e = pcall(
-      vim.api.nvim_exec,
-      "keepjumps buf " .. vim.g.term_toggle_buf,
-      false
-    )
-    if ok == false then
-      vim.notify(e, vim.log.levels.ERROR)
-    end
-    return
+M.term_buf = nil
+
+function M.open_win(max_w, max_h, buf)
+  local scale = 0.4
+
+  local w = math.min(math.max(20, math.floor(max_w * scale)), max_w / 2)
+  if w > 100 then
+    w = 100
   end
 
-  local ok, e = pcall(vim.api.nvim_exec, "keepjumps vertical new", false)
-  if ok == false then
-    vim.notify(e, vim.log.levels.ERROR)
-    return
-  end
-
-  ok, e = pcall(vim.api.nvim_exec, "terminal", false)
-  if ok == false then
-    vim.notify(e, vim.log.levels.ERROR)
-  end
-
-  vim.g.term_toggle_buf = vim.fn.bufnr()
+  local winnr = vim.api.nvim_open_win(buf, true, {
+    anchor = "NE",
+    row = 0,
+    col = max_w,
+    height = max_h,
+    title = "Terminal",
+    noautocmd = true,
+    focusable = true,
+    relative = "editor",
+    style = "minimal",
+    width = w,
+    border = { "│", " ", "", "", "", " ", "│", "│" },
+  })
+  return winnr
 end
+
+function M.create_buf()
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_option(buf, "bufhidden", "hide")
+  return buf
+end
+
+function M.open()
+  if not M.term_buf or not vim.api.nvim_buf_is_valid(M.term_buf) then
+    M.term_buf = M.create_buf()
+  end
+  if not M.term_buf or not vim.api.nvim_buf_is_valid(M.term_buf) then
+    return
+  end
+  local winid = vim.fn.bufwinid(M.term_buf)
+  if winid and winid ~= -1 and vim.api.nvim_win_is_valid(winid) then
+    vim.api.nvim_win_close(winid, false)
+    return
+  end
+  winid = M.open_win(vim.o.columns, vim.o.lines, M.term_buf)
+  vim.api.nvim_win_set_option(
+    winid,
+    "winhl",
+    "FloatBorder:Normal,NormalFloat:Normal,NormalNC:Normal"
+  )
+  vim.api.nvim_buf_call(M.term_buf, function()
+    vim.api.nvim_exec("term", false)
+  end)
+end
+
+return M.open
