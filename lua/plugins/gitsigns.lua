f9c0fcb2869919cdce81a02ac1a4831236d72950
lpoto <luka.potochnik@gmail.com>
2023-01-28 13:05:27 +0100

    Move unception as gitsigns dependency

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -20,35 +20,28 @@ Keymaps:
    - <leader>gu: Unstage current buffer
    - <leader>gr: Reset current buffer
 
-   - <leader>g (or :Git): Git status
-          - <CR> to (un)stage selected file
-          - <C-e> (or e in normal mode) to go to the selected file
+   - <leader>gg Git status
+      see :h telescope.builtin.git_status
    - <leader>gb: Git branches
-          - <CR> to checkout the selected branch
-          - <C-d> to delete the selected branch
-          - <C-t> to track the selected branch
-          - <C-r> to rebase the selected branch
-          - <C-a> to create a new branch
-          - <C-y> to merge the selected branch
+      see :h telescope.builtin.git_branches
    - <leader>gl Git log
-          - <CR> to checkout the selected commit
-          - <C-r>s to reset to the selected commit (soft)
-          - <C-r>h to reset to the selected commit (hard)
-          - <C-r>m to reset to the selected commit (mixed)
+      see :h telescope.builtin.git_commits
    - <leader>gS: Git stash
           - <CR> to apply the selected stash
    - <leader>gf: Git files
    - <leader>gc: Git commit
-   - <leader>ga: Git commit ammend
    - <leader>gP: Git push
    - <leader>gp: Git pull
+   - <leader>g or (:Git) :  Custom git command
 
 --]]
 
 local M = {
   "lewis6991/gitsigns.nvim",
-  keys = "<leader>g",
   event = { "BufNewFile", "BufReadPre" },
+  dependencies = {
+    "samjwill/nvim-unception",
+  },
 }
 
 function M.config()
@@ -74,20 +67,39 @@ function M.config()
       vim.keymap.set("n", "<leader>gr", gs.reset_buffer, opts)
     end,
   }
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "UnceptionEditRequestReceived",
+    callback = function()
+      vim.schedule(function()
+        if vim.bo.buftype ~= "" or vim.bo.filetype:match "^git" then
+          vim.bo.bufhidden = "wipe"
+          vim.bo.swapfile = false
+        end
+      end)
+    end,
+  })
 end
 
+local call_git_command
+
 function M.init()
+  vim.keymap.set("n", "<leader>g", M.git_command, {})
+  vim.api.nvim_create_user_command("Git", function()
+    M.git_command()
+  end, {})
+
   vim.keymap.set("n", "<leader>gl", M.git_commits, {})
   vim.keymap.set("n", "<leader>gb", M.git_branches, {})
-  vim.keymap.set("n", "<leader>g", M.git_status, {})
+  vim.keymap.set("n", "<leader>gg", M.git_status, {})
   vim.keymap.set("n", "<leader>gf", M.git_files, {})
   vim.keymap.set("n", "<leader>gS", M.git_stash, {})
   vim.keymap.set("n", "<leader>gc", M.git_commit, {})
-  vim.keymap.set("n", "<leader>ga", M.git_commit_ammend, {})
   vim.keymap.set("n", "<leader>gP", M.git_push, {})
   vim.keymap.set("n", "<leader>gp", M.git_pull, {})
 
-  vim.api.nvim_create_user_command("Git", M.git_status, {})
+  vim.g.unception_block_while_host_edits = true
+  vim.g.unception_enable_flavor_text = false
 end
 
 M.theme = "ivy"
@@ -122,85 +134,110 @@ function M.git_stash(opts)
   telescope.git_stash(opts)
 end
 
+local attach_git_status_mappings
 function M.git_status(opts)
   local telescope = require "telescope.builtin"
-  local actions = require "telescope.actions"
-
   opts = wrap_opts(opts)
-
-  opts.attach_mappings = function(_, map)
-    actions.select_default:replace(actions.git_staging_toggle)
-    map("n", "e", actions.file_edit)
-    map("i", "<C-e>", actions.file_edit)
-    map("i", "<Tab>", actions.move_selection_next)
-    map("i", "<Tab>", actions.move_selection_next)
-    map("n", "<Tab>", actions.move_selection_next)
-    map("i", "<S-Tab>", actions.move_selection_previous)
-    map("n", "<S-Tab>", actions.move_selection_previous)
-    map("n", "s", actions.git_staging_toggle)
-    map("i", "<C-s>", actions.git_staging_toggle)
-    map("n", "<C-s>", actions.git_staging_toggle)
-    return true
-  end
+  opts.attach_mappings = attach_git_status_mappings
   telescope.git_status(opts)
 end
 
+function M.git_command()
+  call_git_command "git "
+end
+
 function M.git_push()
-  if not vim.g.gitsigns_head or vim.g.gitsigns_head:len() == 0 then
-    vim.notify("No current git HEAD", vim.log.levels.WARN, {
-      title = "Git",
-    })
-    return
-  end
-  local cmd = "git push "
-  local head = vim.fn.input(cmd, "origin " .. vim.g.gitsigns_head)
-  if not head or head:len() == 0 then
-    vim.notify("Invalid git command", vim.log.levels.WARN, {
-      title = "Git",
-    })
-    return
-  end
-  cmd = cmd .. head
-  require("plugins.unception").toggle_term(cmd)
+  call_git_command("git push ", "origin " .. (vim.g.gitsigns_head or ""))
 end
 
 function M.git_pull()
-  if not vim.g.gitsigns_head or vim.g.gitsigns_head:len() == 0 then
-    vim.notify("No current git HEAD", vim.log.levels.WARN, {
-      title = "Git",
-    })
-    return
-  end
-  local cmd = "git pull "
-  local head = vim.fn.input(cmd, "origin " .. vim.g.gitsigns_head)
-  if not head or head:len() == 0 then
-    vim.notify("Invalid git command", vim.log.levels.WARN, {
-      title = "Git",
-    })
-    return
-  end
-  cmd = cmd .. head
-  require("plugins.unception").toggle_term(cmd)
+  call_git_command("git pull ", "origin " .. (vim.g.gitsigns_head or ""))
 end
 
-function M.git_commit_ammend()
-  return M.git_commit(true)
+function M.git_commit()
+  call_git_command("git commit ", "-a")
 end
 
-function M.git_commit(ammend)
-  if not vim.g.gitsigns_head or vim.g.gitsigns_head:len() == 0 then
-    vim.notify("No current git HEAD", vim.log.levels.WARN, {
-      title = "Git",
-    })
-    return
+function attach_git_status_mappings(_, map)
+  local actions = require "telescope.actions"
+  actions.select_default:replace(actions.git_staging_toggle)
+  map("n", "e", actions.file_edit)
+  map("i", "<C-e>", actions.file_edit)
+  map("i", "<Tab>", actions.move_selection_next)
+  map("i", "<Tab>", actions.move_selection_next)
+  map("n", "<Tab>", actions.move_selection_next)
+  map("i", "<S-Tab>", actions.move_selection_previous)
+  map("n", "<S-Tab>", actions.move_selection_previous)
+  map("n", "s", actions.git_staging_toggle)
+  map("i", "<C-s>", actions.git_staging_toggle)
+  map("n", "<C-s>", actions.git_staging_toggle)
+  return true
+end
+
+local run_cmd
+function call_git_command(cmd, suffix)
+  local f = function()
+    if not vim.g.gitsigns_head or vim.g.gitsigns_head:len() == 0 then
+      vim.notify("No git HEAD found", vim.log.levels.WARN, {
+        title = "Git",
+      })
+      return
+    end
+    if suffix then
+      suffix = vim.fn.input(cmd, suffix)
+    else
+      suffix = vim.fn.input(cmd)
+    end
+    if not suffix or suffix:len() == 0 then
+      return
+    end
+    cmd = cmd .. suffix
+    run_cmd(cmd)
   end
-  local cmd = "git commit "
-  if ammend then
-    cmd = cmd .. "--amend"
+  if not package.loaded["gisigns"] then
+    M.config()
+    vim.defer_fn(f, 100)
   else
-    cmd = cmd .. "-a"
+    f()
   end
-  require("plugins.unception").toggle_term(cmd)
+end
+
+function run_cmd(cmd)
+  if type(cmd) ~= "table" and type(cmd) ~= "string" then
+    vim.notify("'cmd' should be a string or a table", vim.log.levels.WARN, {
+      title = "Git Terminal",
+    })
+    return
+  end
+  local new_tab = true
+  if vim.b.terminal_job_id then
+    local r = vim.fn.jobwait({ vim.b.terminal_job_id }, 0)
+    local _, n = next(r)
+    if n == -3 then
+      new_tab = false
+    end
+  end
+  vim.schedule(function()
+    local ok, e = pcall(function()
+      if new_tab then
+        vim.api.nvim_exec("tabnew", false)
+      else
+        vim.api.nvim_buf_set_option(0, "modified", false)
+      end
+      vim.fn.termopen(cmd, {
+        detach = false,
+        env = {
+          VISUAL = "nvim",
+          EDITOR = "nvim",
+        },
+      })
+    end)
+    if not ok and type(e) == "string" then
+      vim.notify(e, vim.log.levels.WARN, {
+        title = "Git Terminal",
+      })
+    end
+  end)
 end
 
 wrap_opts = function(opts)
