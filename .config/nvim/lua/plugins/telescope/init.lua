32f573f682eaba194c1d043beef9cde436311a39
lpoto <luka.potochnik@gmail.com>
2023-01-30 14:43:04 +0100

    disable telescope devicons

--- .config/nvim/lua/plugins/telescope/init.lua
+++ .config/nvim/lua/plugins/telescope/init.lua
@@ -104,7 +104,7 @@ function M.config()
       file_sorter = require("telescope.sorters").get_fzy_sorter,
       generic_sorter = require("telescope.sorters").get_fzy_sorter,
       prompt_prefix = "🔍",
-      color_devicons = true,
+      color_devicons = false,
       file_previewer = require("telescope.previewers").vim_buffer_cat.new,
       grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
       qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
