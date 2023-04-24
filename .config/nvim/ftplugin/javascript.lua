a5801fdf07057f8c9761caed2493d18fa185eb14
lpoto <luka.potochnik@gmail.com>
2023-04-24 17:39:30 +0200

    Add js and ts ftplugins

--- .config/nvim/ftplugin/javascript.lua
+++ .config/nvim/ftplugin/javascript.lua
@@ -1,16 +1,16 @@
 --=============================================================================
 -------------------------------------------------------------------------------
---                                                                     MARKDOWN
+--                                                                   JAVASCRIPT
 --[[===========================================================================
-Loaded when a markdown file is opened
+Loaded when a javascript file is opened
 -----------------------------------------------------------------------------]]
-if vim.g.ftplugin_markdown_loaded then
+if vim.g.ftplugin_javascript_loaded then
   return
 end
-vim.g.ftplugin_markdown_loaded = true
+vim.g.ftplugin_javascript_loaded = true
 
---local lspconfig = require("plugins.lspconfig")
---lspconfig.start_language_server("marksman")
+local lspconfig = require("plugins.lspconfig")
+lspconfig.start_language_server("tsserver")
 
 local null_ls = require "plugins.null-ls"
 null_ls.register_formatter "prettier"
