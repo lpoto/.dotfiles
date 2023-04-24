6b946c6a16d567ce39ec8343a3e80ab76d4cf267
lpoto <luka.potochnik@gmail.com>
2023-04-24 17:35:35 +0200

    Add markdown ftplugin

---
+++ .config/nvim/ftplugin/markdown.lua
@@ -0,0 +1,16 @@
+--=============================================================================
+-------------------------------------------------------------------------------
+--                                                                          LUA
+--[[===========================================================================
+Loaded when a lua file is opened
+-----------------------------------------------------------------------------]]
+if vim.g.ftplugin_markdown_loaded then
+  return
+end
+vim.g.ftplugin_markdown_loaded = true
+
+--local lspconfig = require("plugins.lspconfig")
+--lspconfig.start_language_server("marksman")
+
+local null_ls = require "plugins.null-ls"
+null_ls.register_formatter "prettier"
