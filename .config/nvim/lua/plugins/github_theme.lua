8f9161d80be0bafe7c9d3356f9288f981db26988
lpoto <luka.potochnik@gmail.com>
2023-04-25 22:29:58 +0200

    Replace onedark with github theme

---
+++ .config/nvim/lua/plugins/github_theme.lua
@@ -0,0 +1,33 @@
+--=============================================================================
+-------------------------------------------------------------------------------
+--                                                            GITHUB-NVIM-THEME
+--=============================================================================
+-- https://github.com/projekt0n/github-nvim-theme
+--_____________________________________________________________________________
+
+return {
+  "projekt0n/github-nvim-theme",
+  version = "v0.0.7",
+  lazy = false,
+  config = function()
+    local colorscheme = require "github-theme"
+
+    colorscheme.setup {
+      theme_style = "dark",
+      transparent = true,
+      function_style = "italic",
+      overrides = function()
+        return {
+          Identifier = { link = "Normal" },
+          LineNr = { link = "WinSeparator" },
+          ["@field"] = { link = "Special" },
+          Type = { fg = "#E6BE8A" },
+          ["@type"] = { link = "Type" },
+          StatusLine = { link = "WinSeparator" },
+        }
+      end,
+    }
+
+    vim.cmd.colorscheme "github_dark"
+  end,
+}
