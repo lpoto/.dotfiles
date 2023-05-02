9a6a60d743ce9e5d96f0d1ecb5c90188b27e4f81
lpoto <luka.potochnik@gmail.com>
2023-05-02 19:26:01 +0200

    Make jdtls work for ant projects aswell

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -12,18 +12,25 @@ vim.g.ftplugin_java_loaded = true
 local lspconfig = require "plugins.lspconfig"
 local null_ls = require "plugins.null-ls"
 
--- NOTE: jdtls required java 17 +
+-- NOTE: Ensure you are using java 17 or above while using jdtls, as
+--      it is not compatible with lower versions.
+--
+--      Make sure to often clean the workspace, and java and jdtls
+--      cache (check ~/.jdtls and ~/.cache, ....) to prevent jdtls
+--      from crashing.
 lspconfig.start_language_server("jdtls", {
   cmd = { "jdtls" },
-  root_dir = require("config.util").root_fn({
-    --"build.xml", -- Ant
-    "pom.xml", -- Maven
-    "settings.gradle", -- Gradle
-    "settings.gradle.kts", -- Gradle
-    "mvn", -- mvnw
+  root_dir = require("config.util").root_fn {
+    "build.xml",
+    "pom.xml",
+    "mvn",
+    "settings.gradle",
+    "settings.gradle.kts",
+    "gradlew",
     "build.gradle",
     "build.gradle.kts",
-  }, vim.loop.os_homedir()),
+    ".git",
+  },
 })
 
 null_ls.register_formatter "clang_format"
