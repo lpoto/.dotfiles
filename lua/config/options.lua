6dff4f6ee9831dd175d168c812a4d2c22255617c
lpoto <luka.potochnik@gmail.com>
2023-05-26 18:35:55 +0200

    add leadingmultispace char

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -87,5 +87,9 @@ vim.opt.termguicolors = true
 vim.cmd 'let &t_8f = "\\<Esc>[38;2;%lu;%lu;%lum"'
 vim.cmd 'let &t_8b = "\\<Esc>[48;2;%lu;%lu;%lum"'
 
-vim.opt.listchars:append { tab = "│ ", multispace = ". " }
+vim.opt.listchars:append {
+  tab = "│ ",
+  multispace = ". ",
+  leadmultispace = "│ ",
+}
 vim.opt.list = true
