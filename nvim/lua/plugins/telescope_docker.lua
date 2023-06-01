8160f96ed26518ddbed5c62f69c5b3ad62578d12
lpoto <luka.potochnik@gmail.com>
2023-06-01 18:24:33 +0200

    Update docker config, update plugins

--- nvim/lua/plugins/telescope_docker.lua
+++ nvim/lua/plugins/telescope_docker.lua
@@ -7,8 +7,8 @@ https://github.com/lpoto/telescope-docker.nvim
 Handle docker containers, images and compose files from a prompt.
 
 commands:
-  :Docker {containers|images|compose} - show a list of containers,
-                                       images or compose files
+  :Docker {containers|images|compose|files|machines|swarm} -
+        show a list of containers, images or compose files
 -----------------------------------------------------------------------------]]
 local M = {
   "lpoto/telescope-docker.nvim",
@@ -20,15 +20,19 @@ function M.command()
     if type(opts.args) ~= "string" then
       return
     end
-    local f = "containers"
+    local f = "docker"
     if opts.args:match "^i" then
       f = "images"
     elseif opts.args:match "^f" then
       f = "files"
     elseif opts.args:match "^m" then
       f = "machines"
+    elseif opts.args:match "^s" then
+      f = "swarm"
     elseif opts.args:match "^c.*m" then
       f = "compose"
+    elseif opts.args:match "^c" then
+      f = "containers"
     end
     Util.require("telescope", function(telescope)
       telescope.extensions.docker[f]()
@@ -36,7 +40,8 @@ function M.command()
   end, {
     nargs = "?",
     complete = function(c)
-      local items = { "containers", "images", "compose", "files", "machines" }
+      local items =
+        { "containers", "images", "compose", "files", "machines", "swarm" }
       table.sort(items, function(a, b)
         return Util.string_matching_score(c, a)
           > Util.string_matching_score(c, b)
