cb5c3cab8d6ee1656593c1b9626ec9d1b0079978
Luka Potočnik <luka.potochnik@gmail.com>
2023-06-13 08:25:36 +0200

    Clean un unception config

--- nvim/lua/plugins/unception.lua
+++ nvim/lua/plugins/unception.lua
@@ -6,31 +6,36 @@ https://github.com/samjwill/nvim-unception
 
 Open files from neovim's terminal emulator without nesting sessions.
 
-Add some convenient git commands, as now commit files may be opened in the
+Add some convenient git commands, as now commit files may be opened from the
 terminal emulator.
 
   - <leader>gc - git commit
   - <leader>ga - git commit --amend
   - <leader>gP - git push
+  - <leader>gF - git push --force
   - <leader>gp - git pull
   - <leader>gf - git fetch
+  - <leader>gB - git branch
 ------------------------------------------------------------------------------]]
 local M = {
   "samjwill/nvim-unception",
   event = "VeryLazy",
 }
 
-M.init = function()
+local git_commands = {}
+
+function M.init()
   vim.g.unception_block_while_host_edits = true
   vim.g.unception_enable_flavor_text = false
 
-  vim.keymap.set("n", "<leader>g", M.git_command)
-  vim.keymap.set("n", "<leader>gc", M.git_commit)
-  vim.keymap.set("n", "<leader>ga", M.git_commit_amend)
-  vim.keymap.set("n", "<leader>gp", M.git_pull)
-  vim.keymap.set("n", "<leader>gP", M.git_push)
-  vim.keymap.set("n", "<leader>gF", M.git_push_force)
-  vim.keymap.set("n", "<leader>gf", M.git_fetch)
+  vim.keymap.set("n", "<leader>g", git_commands.default)
+  vim.keymap.set("n", "<leader>gc", git_commands.commit)
+  vim.keymap.set("n", "<leader>ga", git_commands.commit_amend)
+  vim.keymap.set("n", "<leader>gp", git_commands.pull)
+  vim.keymap.set("n", "<leader>gP", git_commands.push)
+  vim.keymap.set("n", "<leader>gF", git_commands.push_force)
+  vim.keymap.set("n", "<leader>gf", git_commands.fetch)
+  vim.keymap.set("n", "<leader>gB", git_commands.branch)
 end
 
 function M.config()
@@ -40,10 +45,6 @@ function M.config()
     callback = function()
       vim.schedule(function()
         pcall(function()
-          -- NOTE: do not save buffers opened
-          -- in the terminal emulator, scratch
-          -- them immediately when they are
-          -- closed.
           vim.bo.bufhidden = "wipe"
           vim.bo.swapfile = false
           vim.bo.buflisted = false
@@ -53,27 +54,27 @@ function M.config()
   })
 end
 
-function M.git_command(suffix)
+function git_commands.default(suffix)
   suffix = suffix or ""
-  M.fetch_git_data(function()
-    Util.require("plugins.unception", function(unception)
-      unception.run_command_with_prompt("git ", suffix)
+  Util.fetch_git_data(function()
+    Util.require("plugins.unception", function(_)
+      Util.run_shell_command_in_tab_with_prompt("git ", suffix)
     end)
   end)
 end
 
-function M.git_commit()
-  M.git_command "commit "
+function git_commands.commit()
+  git_commands.default "commit "
 end
 
-function M.git_commit_amend()
-  M.git_command "commit --amend "
+function git_commands.commit_amend()
+  git_commands.default "commit --amend "
 end
 
-function M.git_push()
-  M.fetch_git_data(function(remote, branch)
-    Util.require("plugins.unception", function(unception)
-      unception.run_command_with_prompt(
+function git_commands.push()
+  Util.fetch_git_data(function(remote, branch)
+    Util.require("plugins.unception", function(_)
+      Util.run_shell_command_in_tab_with_prompt(
         "git ",
         "push " .. remote .. " " .. branch .. " "
       )
@@ -81,10 +82,10 @@ function M.git_push()
   end)
 end
 
-function M.git_push_force()
-  M.fetch_git_data(function(remote, branch)
-    Util.require("plugins.unception", function(unception)
-      unception.run_command_with_prompt(
+function git_commands.push_force()
+  Util.fetch_git_data(function(remote, branch)
+    Util.require("plugins.unception", function(_)
+      Util.run_shell_command_in_tab_with_prompt(
         "git ",
         "push " .. remote .. " " .. branch .. " --force "
       )
@@ -92,107 +93,23 @@ function M.git_push_force()
   end)
 end
 
-function M.git_fetch()
-  M.git_command "fetch "
+function git_commands.fetch()
+  git_commands.default "fetch "
 end
 
-function M.git_pull()
-  M.fetch_git_data(function(remote, branch)
-    Util.require("plugins.unception", function(unception)
-      unception.run_command_with_prompt(
+function git_commands.branch()
+  git_commands.default "branch "
+end
+
+function git_commands.pull()
+  Util.fetch_git_data(function(remote, branch)
+    Util.require("plugins.unception", function(_)
+      Util.run_shell_command_in_tab_with_prompt(
         "git ",
         "pull " .. remote .. " " .. branch .. " "
       )
     end)
   end)
-end
-
----@param cmd string
-function M.run_command(cmd)
-  local new_tab = true
-  if vim.b.terminal_job_id then
-    local r = vim.fn.jobwait({ vim.b.terminal_job_id }, 0)
-    local _, n = next(r)
-    if n == -3 then
-      new_tab = false
-    end
-  end
-  vim.schedule(function()
-    local ok, e = pcall(function()
-      if new_tab then
-        vim.api.nvim_exec("keepjumps tabnew", false)
-      else
-        vim.api.nvim_buf_set_option(0, "modified", false)
-      end
-      vim.bo.bufhidden = "wipe"
-      vim.bo.buflisted = false
-      vim.fn.termopen(cmd, {
-        detach = true,
-        env = {
-          VISUAL = "nvim",
-          EDITOR = "nvim",
-        },
-      })
-    end)
-    if not ok and type(e) == "string" then
-      Util.log():warn(e)
-    end
-  end)
-end
-
-function M.run_command_with_prompt(cmd, suffix)
-  suffix = vim.fn.input {
-    prompt = "$ " .. (cmd or ""),
-    default = suffix,
-    cancelreturn = false,
-  }
-  if type(suffix) ~= "string" then
-    return
-  end
-  M.run_command(cmd .. " " .. suffix)
-end
-
-function M.fetch_git_data(callback, on_error)
-  on_error = on_error
-      or function()
-        Util.log():warn "Could not fetch git data"
-      end
-  local remote = {}
-  vim.fn.jobstart("git remote show", {
-    detach = false,
-    on_stdout = function(_, data)
-      for _, d in ipairs(data) do
-        if d:len() > 0 then
-          table.insert(remote, d)
-        end
-      end
-    end,
-    on_exit = function(_, remote_exit_code)
-      if remote_exit_code ~= 0 then
-        return on_error(remote_exit_code)
-      end
-      local branch = {}
-      vim.fn.jobstart("git branch --show-current", {
-        detach = false,
-        on_stdout = function(_, data)
-          for _, d in ipairs(data) do
-            if d:len() > 0 then
-              table.insert(branch, d)
-            end
-          end
-        end,
-        on_exit = function(_, code)
-          if code ~= 0 then
-            return on_error(code)
-          end
-          return callback(
-            table.concat(remote, " "),
-            table.concat(branch, " ")
-          )
-        end,
-      })
-    end,
-  })
 end
 
 return M
--- nvim/lua/util.lua
+++ nvim/lua/util.lua
@@ -281,15 +281,15 @@ local stdpath = vim.fn.stdpath
 function util.stdpath(what)
   local app_name = os.getenv "NVIM_APPNAME"
   local config = vim.fs.dirname(stdpath "config")
-  local storage = Util.path(config, ".storage")
+  local storage = util.path(config, ".storage")
 
   local n = {
-    config = Util.path(config, app_name),
-    cache = Util.path(storage, "cache", app_name),
-    data = Util.path(storage, "share", app_name),
-    log = Util.path(storage, "log", app_name),
-    run = Util.path(storage, "state", app_name),
-    state = Util.path(storage, "state", app_name),
+    config = util.path(config, app_name),
+    cache = util.path(storage, "cache", app_name),
+    data = util.path(storage, "share", app_name),
+    log = util.path(storage, "log", app_name),
+    run = util.path(storage, "state", app_name),
+    state = util.path(storage, "state", app_name),
     config_dirs = {},
     data_dirs = {},
   }
@@ -366,6 +366,101 @@ function __notify(level, title, delay, ...)
       })
     end
   end, delay)
+end
+
+---@param callback function(remote: string, branch: string)
+---@param on_error nil|function(exit_code: number)
+function util.fetch_git_data(callback, on_error)
+  on_error = on_error
+    or function(_)
+      util.log():warn "Could not fetch git data"
+    end
+  local remote = ""
+  vim.fn.jobstart("git remote show", {
+    detach = false,
+    on_stdout = function(_, data)
+      for _, d in ipairs(data) do
+        if d:len() > 0 then
+          remote = d
+        end
+      end
+    end,
+    on_exit = function(_, remote_exit_code)
+      if remote_exit_code ~= 0 then
+        if type(on_error) == "function" then
+          return on_error()
+        end
+        return
+      end
+      local branch = ""
+      vim.fn.jobstart("git branch --show-current", {
+        detach = false,
+        on_stdout = function(_, data)
+          for _, d in ipairs(data) do
+            if d:len() > 0 then
+              branch = d
+            end
+          end
+        end,
+        on_exit = function(_, code)
+          if code ~= 0 then
+            if type(on_error) == "function" then
+              return on_error()
+            end
+            return
+          end
+          return callback(remote, branch)
+        end,
+      })
+    end,
+  })
+end
+
+---@param cmd string|table
+function util.run_shell_command_in_tab(cmd)
+  local new_tab = true
+  if vim.b.terminal_job_id then
+    local r = vim.fn.jobwait({ vim.b.terminal_job_id }, 0)
+    local _, n = next(r)
+    if n == -3 then
+      new_tab = false
+    end
+  end
+  vim.schedule(function()
+    local ok, e = pcall(function()
+      if new_tab then
+        vim.api.nvim_exec("keepjumps tabnew", false)
+      else
+        vim.api.nvim_buf_set_option(0, "modified", false)
+      end
+      vim.bo.bufhidden = "wipe"
+      vim.bo.buflisted = false
+      vim.fn.termopen(cmd, {
+        detach = true,
+        env = {
+          VISUAL = "nvim",
+          EDITOR = "nvim",
+        },
+      })
+    end)
+    if not ok and type(e) == "string" then
+      util.log():warn(e)
+    end
+  end)
+end
+
+---@param cmd string|table
+---@param suffix string
+function util.run_shell_command_in_tab_with_prompt(cmd, suffix)
+  suffix = vim.fn.input {
+    prompt = "$ " .. (cmd or ""),
+    default = suffix,
+    cancelreturn = false,
+  }
+  if type(suffix) ~= "string" then
+    return
+  end
+  util.run_shell_command_in_tab(cmd .. " " .. suffix)
 end
 
 return util
