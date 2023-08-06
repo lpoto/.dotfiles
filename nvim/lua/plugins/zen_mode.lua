1b21759e55c495ac3653e6857153452e20ed49e8
lpoto <luka.potochnik@gmail.com>
2023-08-06 23:25:34 +0200

    Add zen mode, update plugins

---
+++ nvim/lua/plugins/zen_mode.lua
@@ -0,0 +1,31 @@
+--=============================================================================
+-------------------------------------------------------------------------------
+--                                                                ZEN_MODE.NVIM
+--[[===========================================================================
+https://github.com/folke/zen-mode.nvim
+
+Focus the current window and remove all disctractions
+
+Keymaps:
+  - "<leader>z"  - toggle zen mode
+-----------------------------------------------------------------------------]]
+local M = {
+  "folke/zen-mode.nvim",
+  cmd = "ZenMode",
+  keys = {
+    {
+      "<leader>z",
+      function()
+        vim.api.nvim_exec("ZenMode", false)
+      end,
+      mode = "n",
+    },
+  },
+  opts = {
+    window = {
+      width = 160,
+    },
+  },
+}
+
+return M
