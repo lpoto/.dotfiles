788c65004e2b0e6c4087c890870d10f513b9a049
lpoto <luka.potochnik@gmail.com>
2023-08-17 15:07:12 +0200

    Update flatten config, use floats

--- nvim/lua/plugins/flatten.lua
+++ nvim/lua/plugins/flatten.lua
@@ -25,27 +25,38 @@ local M = {
   lazy = false,
 }
 
-local git_commands = {}
+---@class Git
+local Git = {}
+---@class Shell
+local Shell = {}
 
 function M.config()
-  vim.keymap.set("n", "<leader>g", git_commands.default)
-  vim.keymap.set("n", "<leader>gc", git_commands.commit)
-  vim.keymap.set("n", "<leader>ga", git_commands.commit_amend)
-  vim.keymap.set("n", "<leader>gp", git_commands.pull)
-  vim.keymap.set("n", "<leader>gP", git_commands.push)
-  vim.keymap.set("n", "<leader>gF", git_commands.push_force)
-  vim.keymap.set("n", "<leader>gf", git_commands.fetch)
-  vim.keymap.set("n", "<leader>gt", git_commands.tag)
-  vim.keymap.set("n", "<leader>gB", git_commands.branch)
-  vim.keymap.set("n", "<leader>gr", git_commands.restore)
-  vim.keymap.set("n", "<leader>gs", git_commands.stage)
-  vim.keymap.set("n", "<leader>gu", git_commands.unstage)
+  vim.keymap.set("n", "<leader>gc", Git.commit)
+  vim.keymap.set("n", "<leader>ga", Git.commit_amend)
+  vim.keymap.set("n", "<leader>gp", Git.pull)
+  vim.keymap.set("n", "<leader>gP", Git.push)
+  vim.keymap.set("n", "<leader>gF", Git.push_force)
+  vim.keymap.set("n", "<leader>gf", Git.fetch)
+  vim.keymap.set("n", "<leader>gt", Git.tag)
+  vim.keymap.set("n", "<leader>gB", Git.branch)
+  vim.keymap.set("n", "<leader>gr", Git.restore)
+  vim.keymap.set("n", "<leader>gs", Git.stage)
+  vim.keymap.set("n", "<leader>gu", Git.unstage)
+
+  vim.api.nvim_create_user_command("Git", function(opts)
+    Git:default({
+      suffix = opts.args,
+      ask_for_input = opts.args:len() == 0,
+    })
+  end, {
+    nargs = "*",
+  })
 
   Util.require("flatten", function(flatten)
     flatten.setup({
       callbacks = {
         should_block = function()
-          return false
+          return true
         end,
         should_nest = function()
           return false
@@ -53,49 +64,55 @@ function M.config()
       },
       block_for = {},
       window = {
-        open = "tab",
         diff = "tab_vsplit",
+        open = function(files, _, stdin_buf)
+          local focus = files[1] or files[#files]
+          if stdin_buf then
+            focus = stdin_buf
+          end
+          local buf, win = Shell:open_float(" " .. (focus.fname or "") .. " ")
+          vim.api.nvim_set_current_buf(focus.bufnr)
+          buf = focus.bufnr
+          return buf, win
+        end,
       },
     })
   end)
 end
 
----@class ShellUtil
-local Shell = {}
-
-function git_commands.commit()
-  git_commands.default("commit ")
+function Git:commit()
+  Git:default("commit ")
 end
 
-function git_commands.commit_amend()
-  git_commands.default("commit --amend ")
+function Git:commit_amend()
+  Git:default("commit --amend ")
 end
 
-function git_commands.push()
+function Git:push()
   Shell:fetch_git_data(function(remote, branch)
-    git_commands.default("push " .. remote .. " " .. branch .. " ")
+    Git:default("push " .. remote .. " " .. branch .. " ")
   end)
 end
 
-function git_commands.push_force()
+function Git:push_force()
   Shell:fetch_git_data(function(remote, branch)
-    git_commands.default("push " .. remote .. " " .. branch .. " --force ")
+    Git:default("push " .. remote .. " " .. branch .. " --force ")
   end)
 end
 
-function git_commands.fetch()
-  git_commands.default("fetch ")
+function Git:fetch()
+  Git:default("fetch ")
 end
 
-function git_commands.branch()
-  git_commands.default("branch ")
+function Git:branch()
+  Git:default("branch ")
 end
 
-function git_commands.tag()
-  git_commands.default("tag ")
+function Git:tag()
+  Git:default("tag ")
 end
 
-function git_commands.restore()
+function Git:restore()
   if vim.bo.modified then
     local ok, _ = pcall(vim.api.nvim_exec, "earlier 1f", false)
     if not ok then
@@ -105,172 +122,126 @@ function git_commands.restore()
     end
   end
   local custom_success_message = "Successfully restored current file"
-  git_commands.default(
-    "restore " .. vim.fn.expand("%:p") .. " ",
-    false,
-    custom_success_message,
-    true
-  )
+  Git:default({
+    suffix = "restore " .. vim.fn.expand("%:p") .. " ",
+    ask_for_input = false,
+    custom_success_message = custom_success_message,
+    edit_after_end = true,
+  })
 end
 
-function git_commands.stage()
+function Git:stage()
   local custom_success_message = "Successfully staged current file"
-  git_commands.default(
-    "stage " .. vim.fn.expand("%:p") .. " ",
-    false,
-    custom_success_message
-  )
+  Git:default({
+    suffix = "stage " .. vim.fn.expand("%:p") .. " ",
+    ask_for_input = false,
+    custom_success_message = custom_success_message,
+  })
 end
 
-function git_commands.unstage()
+function Git:unstage()
   local custom_success_message = "Successfully unstaged current file"
-  git_commands.default(
-    "reset " .. vim.fn.expand("%:p") .. " ",
-    false,
-    custom_success_message
-  )
+  Git:default({
+    suffix = "reset " .. vim.fn.expand("%:p") .. " ",
+    ask_for_input = false,
+    custom_success_message = custom_success_message,
+  })
 end
 
-function git_commands.pull()
+function Git:pull()
   Shell:fetch_git_data(function(remote, branch)
-    git_commands.default("pull " .. remote .. " " .. branch .. " ")
+    Git:default({ suffix = "pull " .. remote .. " " .. branch .. " " })
   end)
 end
 
-function git_commands.default(
-  suffix,
-  ask_for_input,
-  custom_success_message,
-  edit_after_end
-)
+function Git:default(opts)
+  if type(opts) == "string" then
+    opts = { suffix = opts }
+  end
+  if type(opts) ~= "table" then
+    opts = {}
+  end
   local cur_buf = vim.api.nvim_get_current_buf()
 
-  local cur_term_win = nil
   local bufs = vim.api.nvim_list_bufs()
 
-  if ask_for_input == nil then
-    ask_for_input = true
+  if opts.ask_for_input == nil then
+    opts.ask_for_input = true
   end
-  suffix = suffix or ""
+  if type(opts.suffix) ~= "string" then
+    opts.suffix = ""
+  end
   Shell:fetch_git_data(function()
-    if ask_for_input then
-      suffix = vim.fn.input({
-        prompt = "git ",
-        default = suffix,
-        cancelreturn = "",
-      })
-      if suffix == "" then
-        return
-      end
-    end
-    local cmd = "git " .. suffix
-    vim.api.nvim_exec("noautocmd keepjumps tabnew", false)
-    cur_term_win = vim.api.nvim_get_current_win()
-    vim.fn.termopen(cmd, {
-      detach = false,
-      on_exit = function(_, code)
-        vim.schedule(function()
-          pcall(function()
-            for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-              if
-                vim.api.nvim_buf_get_name(buf) == ""
-                and vim.api.nvim_buf_get_option(buf, "filetype") == ""
-                and vim.api.nvim_buf_get_option(buf, "buftype") == ""
-                and not vim.tbl_contains(bufs, buf)
-              then
-                pcall(vim.api.nvim_buf_delete, buf, { force = true })
-              end
+    local o = {
+      cmd = "git ",
+      prompt = opts.ask_for_input,
+      suffix = opts.suffix or "",
+    }
+    o.on_exit = function(_, code)
+      vim.schedule(function()
+        pcall(function()
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            if
+              vim.api.nvim_buf_get_name(buf) == ""
+              and vim.api.nvim_buf_get_option(buf, "filetype") == ""
+              and vim.api.nvim_buf_get_option(buf, "buftype") == ""
+              and not vim.tbl_contains(bufs, buf)
+            then
+              pcall(vim.api.nvim_buf_delete, buf, { force = true })
             end
-          end)
+          end
         end)
+      end)
+      if o.win ~= nil and vim.api.nvim_win_is_valid(o.win) then
+        local cur_term = vim.api.nvim_win_get_buf(o.win)
+        local lines = vim.tbl_filter(function(el)
+          return #el > 0
+        end, vim.api.nvim_buf_get_lines(cur_term, 0, -1, false))
+        local log = function(...)
+          local log_name = "Git"
+          if code == 0 then
+            Util.log({ delay = 50, title = log_name }):info(...)
+          else
+            Util.log({ delay = 50, title = log_name }):warn(...)
+          end
+        end
+        local do_delete = false
         if
-          cur_term_win ~= nil and vim.api.nvim_win_is_valid(cur_term_win)
+          code == 0
+          and type(opts.custom_success_message) == "string"
+          and opts.custom_success_message:len() > 0
         then
-          local cur_term = vim.api.nvim_win_get_buf(cur_term_win)
-          local lines = vim.tbl_filter(function(el)
-            return #el > 0
-          end, vim.api.nvim_buf_get_lines(cur_term, 0, -1, false))
-          local log = function(...)
-            local log_name = "Git"
-            if code == 0 then
-              Util.log({ delay = 50, title = log_name }):info(...)
-            else
-              Util.log({ delay = 50, title = log_name }):warn(...)
-            end
+          do_delete = true
+          log(opts.custom_success_message)
+        elseif #lines == 0 then
+          if code == 0 then
+            log(o.cmd, "SUCCESS")
+          else
+            log(o.cmd, "exited with code", code)
           end
-          if
-            code == 0
-            and type(custom_success_message) == "string"
-            and custom_success_message:len() > 0
-          then
-            log(custom_success_message)
-          elseif #lines == 0 then
-            if code == 0 then
-              log(cmd, "SUCCESS")
-            else
-              log(cmd, "exited with code", code)
-            end
-          elseif #lines < 10 and #vim.api.nvim_list_tabpages() > 1 then
-            log(table.concat(lines, "\n"))
-          end
-          vim.api.nvim_win_close(cur_term_win, true)
+          do_delete = true
+        elseif opts.log_short_messages and #lines < 10 then
+          log(table.concat(lines, "\n"))
+          do_delete = true
+        end
+        if do_delete then
+          vim.api.nvim_win_close(o.win, true)
           vim.api.nvim_buf_delete(cur_term, { force = true })
+          o.win = nil
+          o.buf = nil
         end
-        cur_term_win = nil
-        if vim.api.nvim_get_current_buf() == cur_buf and edit_after_end then
-          vim.api.nvim_exec("e", true)
-        end
-      end,
-    })
-  end)
-end
-
----@param cmd string|table
-function Shell:run_in_tab(cmd)
-  local new_tab = true
-  if vim.b.terminal_job_id then
-    local r = vim.fn.jobwait({ vim.b.terminal_job_id }, 0)
-    local _, n = next(r)
-    if n == -3 then
-      new_tab = false
-    end
-  end
-  vim.schedule(function()
-    local ok, e = pcall(function()
-      if new_tab then
-        vim.api.nvim_exec("keepjumps tabnew", false)
-      else
-        vim.api.nvim_buf_set_option(0, "modified", false)
       end
-      vim.bo.bufhidden = "wipe"
-      vim.bo.buflisted = false
-      vim.fn.termopen(cmd, {
-        detach = true,
-        env = {
-          VISUAL = "nvim",
-          EDITOR = "nvim",
-        },
-      })
-    end)
-    if not ok and type(e) == "string" then
-      Util.log():warn(e)
+      if
+        vim.api.nvim_get_current_buf() == cur_buf and opts.edit_after_end
+      then
+        vim.api.nvim_exec("e", true)
+      end
     end
+    Shell:run_in_float(o)
   end)
 end
 
----@param cmd string|table
----@param suffix string
-function Shell:run_in_tab_with_prompt(cmd, suffix)
-  suffix = vim.fn.input({
-    prompt = "$ " .. (cmd or ""),
-    default = suffix,
-    cancelreturn = false,
-  })
-  if type(suffix) ~= "string" then
-    return
-  end
-  self:run_in_tab(cmd .. " " .. suffix)
-end
+local shell_augroup = "ShellAugroup"
 
 ---@param callback function(remote: string, branch: string)
 ---@param on_error nil|function(exit_code: number)
@@ -318,6 +289,145 @@ function Shell:fetch_git_data(callback, on_error)
       })
     end,
   })
+end
+
+local term_winid = nil
+function Shell:run_in_float(opts)
+  if type(opts) ~= "table" then
+    opts = {}
+  end
+  if type(opts.cmd) ~= "string" then
+    Util.log("Shell"):warn("The command should be a string")
+    return
+  end
+  if opts.prompt then
+    opts.suffix = vim.fn.input({
+      prompt = "$ " .. (opts.cmd or ""),
+      default = opts.suffix,
+      cancelreturn = false,
+      completion = "file",
+    })
+    if type(opts.suffix) ~= "string" then
+      return
+    end
+  end
+  if type(opts.suffix) == "string" then
+    opts.cmd = opts.cmd:gsub("%s+$", "") .. " " .. opts.suffix
+  end
+  pcall(vim.api.nvim_clear_autocmds, {
+    group = shell_augroup,
+  })
+  if
+    type(term_winid) == "number" and vim.api.nvim_win_is_valid(term_winid)
+  then
+    vim.fn.win_gotoid(term_winid)
+    opts.buf = vim.api.nvim_get_current_buf()
+    opts.win = vim.api.nvim_get_current_win()
+    pcall(function()
+      local config = vim.api.nvim_win_get_config(opts.win)
+      config.title = " " .. opts.cmd .. " "
+      vim.api.nvim_win_set_config(opts.win, config)
+    end)
+    vim.bo.modified = false
+  else
+    opts.buf, opts.win = Shell:open_float(" " .. opts.cmd .. " ")
+    term_winid = opts.win
+  end
+  local term_opts = {
+    detach = false,
+    env = {
+      VISUAL = "nvim",
+      EDITOR = "nvim",
+    },
+    on_exit = function(...)
+      if type(opts.on_exit) == "function" then
+        opts.on_exit(...)
+      end
+      if
+        vim.api.nvim_get_current_win() == opts.win
+        and vim.bo.buftype == "terminal"
+      then
+        local delete = function()
+          pcall(function()
+            local buf = vim.api.nvim_get_current_buf()
+            vim.api.nvim_win_close(opts.win, true)
+            vim.api.nvim_buf_delete(buf, { force = true })
+          end)
+        end
+        pcall(function()
+          for _, k in ipairs({ "q", "<Esc>" }) do
+            vim.keymap.set(
+              "n",
+              k,
+              delete,
+              { buffer = vim.api.nvim_get_current_buf() }
+            )
+          end
+          vim.api.nvim_create_autocmd("WinLeave", {
+            group = vim.api.nvim_create_augroup(
+              shell_augroup,
+              { clear = true }
+            ),
+            buffer = vim.api.nvim_get_current_buf(),
+            callback = delete,
+          })
+        end)
+      end
+    end,
+  }
+  if type(opts.on_stdout) == "function" then
+    term_opts.on_stdout = opts.on_stdout
+  end
+  if type(opts.on_stderr) == "function" then
+    term_opts.on_stderr = opts.on_stderr
+  end
+  vim.fn.termopen(opts.cmd, term_opts)
+end
+
+function Shell:open_float(title)
+  local buf = vim.api.nvim_create_buf(false, true)
+
+  local win = vim.api.nvim_open_win(
+    buf,
+    true,
+    vim.tbl_extend(
+      "force",
+      Shell:get_centered_float_opts(vim.o.lines, vim.o.columns, 0.6),
+      {
+        relative = "editor",
+        style = "minimal",
+        border = "rounded",
+        title = title,
+        title_pos = "center",
+      }
+    )
+  )
+  return buf, win
+end
+
+function Shell:get_centered_float_opts(lines, columns, scale)
+  local pref_w = math.floor(columns * scale)
+  local w = math.max(math.min(pref_w, columns), 100)
+  local h = math.max(lines - 5, 12)
+
+  local row = 1
+  local col = (columns - w) / 2
+  if columns % 2 ~= 0 then
+    col = col - 1
+  end
+  if w == columns then
+    col = 0
+  end
+  if h == lines then
+    row = 0
+  end
+
+  return {
+    width = w,
+    height = h,
+    row = row,
+    col = col,
+  }
 end
 
 return M
