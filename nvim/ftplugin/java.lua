f402b2405886a9e27af5fea4fdbbb0a2e0b71464
Luka Potočnik <luka.potochnik@gmail.com>
2023-09-05 16:56:24 +0200

    clean up java config

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -5,22 +5,15 @@
 if vim.g[vim.bo.filetype] or vim.api.nvim_set_var(vim.bo.filetype, true) then
   return
 end
+local cache = vim.fn.stdpath("cache") .. "/jdtls/workspace" .. vim.fn.getcwd()
 
 require("lsp"):attach("clangformat")
 require("lsp"):attach({
+  ---NOTE: Ensure you are using java 17 or above while using jdtls
+  --NOTE: Clean up the cache directory from time to time
   name = "jdtls",
-  ---NOTE: Ensure you are using java 17 or above while using jdtls, as
-  ---    it is not compatible with lower versions.
-  ---
-  ---     Make sure to often clean the workspace, and java and jdtls
-  ---     cache (vim.fn.stdpath("cache") .. "/jdtls") to prevent jdtls
-  ---     from crashing.
-  cmd = {
-    "jdtls",
-    "-data",
-
-    vim.fn.stdpath("cache") .. "/jdtls/workspace/" .. vim.fn.getcwd(),
-  },
+  cmd = { "jdtls", "-data", cache },
+  settings = vim.g.jdtls_settings,
   root_patterns = {
     "build.xml",
     "pom.xml",
@@ -32,24 +25,4 @@ require("lsp"):attach({
     "build.gradle.kts",
     ".git",
   },
-  --[[
-
-    NOTE: jdtls will work fine with gradle and maven, but it might
-          not work with ant or other older build tools.
-
-          As a workaround, you may add something similar to a project's
-          local config to manually add the required libraries:
-
-          vim.g.jdtls_config = {
-            settings = {
-              java = {
-                project = {
-                  referencedLibraries = {
-                    "/path_to_project/lib/**",
-                  }
-                }
-              }
-            }
-          }
-    --]]
 })
