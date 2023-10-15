2120d998906d37ff97093c95a08575cda88377d7
lpoto <luka.potocnik@icloud.com>
2023-10-15 10:48:16 +0200

    just pylsp for formatting python files

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -6,6 +6,4 @@ if vim.g[vim.bo.filetype] or vim.api.nvim_set_var(vim.bo.filetype, true) then
   return
 end
 
-vim.lsp.attach 'autopep8'
-vim.lsp.attach 'pyright'
-vim.lsp.attach 'flake8'
+vim.lsp.attach 'pylsp'
