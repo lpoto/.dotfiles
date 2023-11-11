045eb89ee29cb18d0cdac701dc1a4337f7147701
lpoto <luka.potocnik@icloud.com>
2023-11-11 05:02:13 +0100

    store different shada files based on cwd

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -144,3 +144,34 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end
   end,
 })
+
+------------------------------------------------------------------------- Shada
+-- Store different shada files based on current working directory
+
+local function shada_autocmd(write, read)
+  if write then
+    pcall(vim.api.nvim_exec2, 'wshada!', {})
+  end
+  local tail = ((vim.fn.expand '%:p:h') .. '.shada'):gsub('/', '%%')
+  local file = vim.fn.stdpath 'data' .. '/shada/' .. tail
+  vim.opt.shada = {
+    '!',
+    "'100",
+    '<50',
+    's10',
+    'h',
+    'n' .. file
+  }
+  if read then
+    pcall(vim.api.nvim_exec2, 'rshada!', {})
+  end
+end
+vim.api.nvim_create_autocmd('DirChanged', {
+  group = vim.api.nvim_create_augroup('ShadaGroup', { clear = true }),
+  callback = function()
+    vim.schedule(function()
+      shada_autocmd(true, true)
+    end)
+  end,
+})
+shada_autocmd(false, false)
